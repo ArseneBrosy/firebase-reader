28ea7fca38cd70db975f0ab2cd149c7c78e3dae9
ArseneBrosy <arsene.brosy@divtec.ch>
2023-10-26 22:27:24 +0200

    js 1

--- script.js
+++ script.js
@@ -6,5 +6,32 @@ let dbURL = "https://spider-shooter-game-default-rtdb.europe-west1.firebasedatab
 let database = getDatabase(initializeApp({databaseURL: dbURL}));
 
 onValue(ref(database), (snapshot) => {
-  console.log(snapshot.val());
+  const JSONValue = JSON.stringify(snapshot.val());
+  let HTMLElements = "";
+  console.log(JSONValue);
+
+  // construct the HTML
+  let currentPropOrValue = "";
+  let i = 0;
+  let objectId = 0;
+  for (let char of JSONValue) {
+    if (["\"", "{", "}", ":", ","].includes(char)) {
+      // is structure char
+      if (currentPropOrValue !== "") {
+        let mode = "prop";
+        if (JSONValue[i + 2] === "{") {
+          // is an object
+          mode = "object";
+          HTMLElements += `<div class="object opened" id="object-${objectId}"><div class="line"><button onclick="openCloseObject('object-${objectId}')"><span class="material-symbols-outlined">arrow_right</span></button><a href="">${currentPropOrValue}</a></div>`;
+          objectId++;
+        }
+        console.log(`${mode}: ${currentPropOrValue}`);
+        currentPropOrValue = "";
+      }
+    } else {
+      // is a prop or value char
+      currentPropOrValue += char;
+    }
+    i++;
+  }
 });
